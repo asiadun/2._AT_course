68090162c2d9d4d23c85b73b21245da2a547b919
a.siadun <a.siadun@outlook.com>
2021-08-03 15:31:45 +0300

    Add assert

--- 1. Section 2 - Browser Automation Basics.py
+++ 1. Section 2 - Browser Automation Basics.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 
 browser = webdriver.Chrome()
@@ -8,9 +9,17 @@ input2_css_locator = "input[id='ipt2']"
 button4_xpath_locator = "//button[@id='b4']"
 
 #Assign elements
-input1_elem = browser.find_element_by_css_selector(input2_css_locator)
-butn4_elem = browser.find_element_by_xpath(button4_xpath_locator)
+def Test_1():
+    input1_elem = browser.find_element_by_css_selector(input2_css_locator)
+    assert True
+
+def Test_2():
+    butn4_elem = browser.find_element_by_xpath(button4_xpath_locator)
+    assert True
 
 # Manipulate elements
 input1_elem.send_keys("Test text")
 butn4_elem.click()
+assert input1_elem == input2_css_locator
+assert input1_elem == "input[id='ipt2']"
+assert butn4_elem == "//button[@id='b4']"
