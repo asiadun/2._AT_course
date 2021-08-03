54ee1d5659bf681a25678efc47f3b72517be2618
a.siadun <a.siadun@outlook.com>
2021-08-03 15:43:51 +0300

    update assert

--- 1. Section 2 - Browser Automation Basics.py
+++ 1. Section 2 - Browser Automation Basics.py
@@ -9,17 +9,17 @@ input2_css_locator = "input[id='ipt2']"
 button4_xpath_locator = "//button[@id='b4']"
 
 #Assign elements
-def Test_1():
+def Test_1(self):
     input1_elem = browser.find_element_by_css_selector(input2_css_locator)
+    input1_elem.send_keys("Test text")
     assert True
+    assert input1_elem == input2_css_locator
+    assert input1_elem == "ipt2"
 
 def Test_2():
     butn4_elem = browser.find_element_by_xpath(button4_xpath_locator)
+    butn4_elem.click()
     assert True
+    assert butn4_elem == "//button[@id='b4']"
 
 # Manipulate elements
-input1_elem.send_keys("Test text")
-butn4_elem.click()
-assert input1_elem == input2_css_locator
-assert input1_elem == "input[id='ipt2']"
-assert butn4_elem == "//button[@id='b4']"
